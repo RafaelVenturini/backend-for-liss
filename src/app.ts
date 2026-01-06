7f549502867998aa58fb62f90b09fe305b8afde4
Rafael <rafaelventurinidipalma@gmail.com>
2026-01-06 15:16:29 -0300

    commit de debug II

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,19 @@ import database from "@plugins/database.js";
 export function buildApp() {
 	const app = fastify({logger: true});
 	
+	app.setErrorHandler((error, request, reply) => {
+		request.log.error({
+			msg: "Error catched in global error handler",
+			error: error,
+			payload: request.body
+		})
+		
+		reply.status(500).send({
+			error: 'Internal Server Error',
+			msg: error
+		})
+	})
+	
 	app.register(database)
 	registerRoutes(app)
 	return app
