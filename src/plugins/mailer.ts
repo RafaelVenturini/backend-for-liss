6b2bff7f2ef7de5a9d9a342420819cc6bb5ad176
Rafael <rafaelventurinidipalma@gmail.com>
2026-01-13 08:08:02 -0300

    pequeno debug do sistema II

--- src/plugins/mailer.ts
+++ src/plugins/mailer.ts
@@ -8,11 +8,26 @@ import {fitnessMailer} from "@emails/emails.js";
 
 const mailerPlugin: FastifyPluginAsync = async (app: FastifyInstance) => {
 	const transporter = nodemailer.createTransport(fitnessMailer);
+	
+	const verifyPromise = transporter.verify();
+	const timeoutPromise = new Promise((_, reject) =>
+		setTimeout(() => reject(new Error('SMTP verify timeout')), 5000)
+	);
+	
 	try {
-		await transporter.verify();
-	} catch (e) {
-		console.log(e);
+		await Promise.race([verifyPromise, timeoutPromise]);
+	} catch (error) {
+		console.error('\n\n\n\nErro ao verificar o transporte de' +
+			' e-mail:', error, '\n\n\n\n');
+		// Não lance o erro, apenas logue
 	}
+	
+	// try {
+	// 	await transporter.verify();
+	//
+	// } catch (e) {
+	// 	console.log(e);
+	// }
 	
 	const sendEmail = async (templateName: string, subject: string, to: string, data: any) => {
 		const filePath = path.join(process.cwd(), 'src/templates/emails', `${templateName}.html`);
