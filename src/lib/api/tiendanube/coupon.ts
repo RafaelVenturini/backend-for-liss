9a34fb4e8bfd043296068eb5dc7e67cc787b3c50
Rafael Venturini <rafaelventurinidipalma@gmail.com>
2026-01-27 15:03:40 -0300

    corrigido erro inesperado de cupons

--- src/lib/api/tiendanube/coupon.ts
+++ src/lib/api/tiendanube/coupon.ts
@@ -1,81 +1,83 @@
-import {Coupon} from "@api/tiendanube/interfaces.js";
-import {appConfig} from "@config";
+import { Coupon } from "@api/tiendanube/interfaces.js";
+import { appConfig } from "@config";
 
 interface Opt {
-	headers: {
-		'Content-Type': string;
-		Authentication: string;
-		'User-Agent': string;
-	};
-	method: string
-	body: any | null
+  headers: {
+    "Content-Type": string;
+    Authentication: string;
+    "User-Agent": string;
+  };
+  method: string;
+  body: any | null;
 }
 
-const url = 'https://api.tiendanube.com/v1/4820240/coupons'
+const url = "https://api.tiendanube.com/v1/4820240/coupons";
 const opt: Opt = {
-	headers: appConfig.nuvemshop.headers,
-	method: '',
-	body: null
-}
+  headers: appConfig.nuvemshop.headers,
+  method: "",
+  body: null,
+};
 
 interface CouponItens {
-	couponCode: string
-	discount: string
-	startDate: Date
-	vality: Date
+  couponCode: string;
+  discount: string;
+  startDate: string;
+  vality: Date;
 }
 
 function formatBody(couponItens: CouponItens) {
-	return {
-		code: couponItens.couponCode,
-		type: 'absolute',
-		value: Number(couponItens.discount),
-		start_date: couponItens.startDate.toISOString().split('T')[0],
-		end_date: couponItens.vality.toISOString().split('T')[0],
-		max_uses: 1
-	}
+  return JSON.stringify({
+    code: couponItens.couponCode,
+    type: "absolute",
+    value: Number(couponItens.discount),
+    start_date: couponItens.startDate.split("T")[0],
+    end_date: couponItens.vality.toISOString().split("T")[0],
+    max_uses: 1,
+  });
 }
 
 export async function getCoupon(id: number): Promise<Coupon[]> {
-	opt.method = 'GET'
-	opt.body = null
-	const resp = await fetch(`${url}?q=${id}`, opt)
-	return await resp.json()
+  opt.method = "GET";
+  opt.body = null;
+  const resp = await fetch(`${url}?q=${id}`, opt);
+  return await resp.json();
 }
 
 export async function createCoupon(couponItens: CouponItens) {
-	opt.method = 'POST'
-	opt.body = formatBody(couponItens)
-	
-	const resp = await fetch(url, opt)
-	return resp.json()
+  opt.method = "POST";
+  opt.body = formatBody(couponItens);
+
+  console.log(opt);
+  console.log(url);
+  const resp = await fetch(url, opt);
+  return resp.json();
 }
 
 export async function updateCoupon(id: number, couponItens: CouponItens) {
-	opt.method = 'PUT'
-	opt.body = formatBody(couponItens)
-	
-	const resp = await fetch(`${url}/${id}`, opt)
-	return resp.json()
+  opt.method = "PUT";
+  opt.body = formatBody(couponItens);
+
+  const resp = await fetch(`${url}/${id}`, opt);
+  return resp.json();
 }
 
 export async function deleteCoupon(id: number) {
-	opt.method = 'DELETE'
-	opt.body = null
-	const resp = await fetch(`${url}/${id}`, opt)
-	return resp.json()
+  opt.method = "DELETE";
+  opt.body = null;
+  const resp = await fetch(`${url}/${id}`, opt);
+  return resp.json();
 }
 
 export async function clearCoupons() {
-	opt.method = 'GET'
-	opt.body = null
-	const resp = await fetch(`${url}?status=deactivated&per_page=200`, opt)
-	const data: Coupon[] = await resp.json()
-	let count = 0
-	
-	for (const coupon of data) {
-		await deleteCoupon(coupon.id)
-		count++
-	}
-	return count
+  opt.method = "GET";
+  opt.body = null;
+  const resp = await fetch(`${url}?status=deactivated&per_page=200`, opt);
+  const data: Coupon[] = await resp.json();
+  let count = 0;
+
+  for (const coupon of data) {
+    await deleteCoupon(coupon.id);
+    count++;
+  }
+  return count;
 }
--- src/routes/api/tiendanube/coupon/post.ts
+++ src/routes/api/tiendanube/coupon/post.ts
@@ -1,50 +1,58 @@
-import {RouteHandlerMethod} from "fastify";
+import { RouteHandlerMethod } from "fastify";
 import {
-	createCoupon,
-	deleteCoupon,
-	getCoupon,
-	updateCoupon
+  createCoupon,
+  deleteCoupon,
+  getCoupon,
+  updateCoupon,
 } from "@api/tiendanube/coupon.js";
-import {cashBack, cashbackValityDays} from "@/lib/util/busines-rules.js";
-import {calculateDate} from "@/lib/string/date.js";
+import { cashBack, cashbackValityDays } from "@/lib/util/busines-rules.js";
+import { calculateDate } from "@/lib/string/date.js";
+import { error } from "node:console";
 
 const postCoupon: RouteHandlerMethod = async (request, reply) => {
-	const {customer, order, value, startDate, event} = request.body as any
-	const couponCode = `CB${customer}${order}`
-	const discount = (value * cashBack).toFixed(2)
-	
-	const vality = calculateDate(new Date(startDate), cashbackValityDays)
-	
-	const couponItens = {
-		couponCode,
-		discount,
-		startDate,
-		vality,
-	}
-	let couponSearch
-	let couponId
-	switch (event) {
-		case 'order/created':
-			await createCoupon(couponItens)
-			return reply.status(201).send({data: couponItens})
-		case 'order/cancelled':
-			couponSearch = await getCoupon(order)
-			if (!couponSearch[0]) return reply.status(404).send({error: 'Coupon not found'})
-			couponId = couponSearch[0].id
-			if (!couponId) return reply.status(404).send({error: 'Coupon not found'})
-			await deleteCoupon(couponId)
-			return reply.status(204).send({data: 'Coupon deleted successfully'})
-		case 'order/updated':
-		case 'order/edited':
-			couponSearch = await getCoupon(order)
-			couponId = couponSearch[0].id
-			if (!couponId) return reply.status(404).send({error: 'Coupon not found'})
-			const resp = await updateCoupon(couponId, couponItens)
-			if (!resp) return reply.status(404).send({error: 'Coupon not found'})
-			return reply.status(201).send({data: resp})
-		default:
-			return reply.status(204).send({data: 'No action'})
-	}
-}
+  const { customer, order, value, startDate, event } = request.body as any;
+  const couponCode = `CB${customer}${order}`;
+  const discount = (value * cashBack).toFixed(2);
+
+  const vality = calculateDate(new Date(startDate), cashbackValityDays);
+
+  const couponItens = {
+    couponCode,
+    discount,
+    startDate,
+    vality,
+  };
+  let couponSearch;
+  let couponId;
+
+  switch (event) {
+    case "order/created":
+      const response = await createCoupon(couponItens);
+      if (response.code > 300) {
+        return reply.status(500).send({ error: JSON.stringify(response) });
+      }
+      return reply.status(201).send({ data: { couponItens, response } });
+    case "order/cancelled":
+      couponSearch = await getCoupon(order);
+      if (!couponSearch[0])
+        return reply.status(404).send({ error: "Coupon not found" });
+      couponId = couponSearch[0].id;
+      if (!couponId)
+        return reply.status(404).send({ error: "Coupon not found" });
+      await deleteCoupon(couponId);
+      return reply.status(204).send({ data: "Coupon deleted successfully" });
+    case "order/updated":
+    case "order/edited":
+      couponSearch = await getCoupon(order);
+      couponId = couponSearch[0].id;
+      if (!couponId)
+        return reply.status(404).send({ error: "Coupon not found" });
+      const resp = await updateCoupon(couponId, couponItens);
+      if (!resp) return reply.status(404).send({ error: "Coupon not found" });
+      return reply.status(201).send({ data: resp });
+    default:
+      return reply.status(204).send({ data: "No action" });
+  }
+};
 
 export default postCoupon;
