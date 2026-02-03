fcf251271d3b33f0c227cd4da48ddc602930d700
Rafael Venturini <rafaelventurinidipalma@gmail.com>
2026-02-03 16:21:01 -0300

    Revert "criado algumas rotas de Health"
    This reverts commit 7fe08b355748cf19e010a31fb8eea962d3244c78.
    
    papai do ceu em ti confio

--- src/routes/health/nuvemshop/customer/index.ts
+++
@@ -1,9 +0,0 @@
-import {FastifyPluginAsync} from "fastify";
-import getCustomerHealth from "@routes/health/nuvemshop/customer/get.js";
-import {HealthSchema} from "@routes/health/schema.js";
-
-const customerHealthRoutes: FastifyPluginAsync = async (app) => {
-    app.get('', {schema: HealthSchema}, getCustomerHealth)
-}
-
-export default customerHealthRoutes;
--- src/routes/health/nuvemshop/index.ts
+++
@@ -1,8 +0,0 @@
-import {FastifyPluginAsync} from "fastify";
-import customerHealthRoutes from "@routes/health/nuvemshop/customer/index.js";
-
-const nuvemshopRoutes: FastifyPluginAsync = async (app) => {
-    app.register(customerHealthRoutes, {prefix: '/customer'})
-}
-
-export default nuvemshopRoutes;
