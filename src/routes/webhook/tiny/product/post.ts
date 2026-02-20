7c0b512d5e4f52986c6c5066655d153220e0ac41
Rafael Venturini <rafaelventurinidipalma@gmail.com>
2026-02-20 15:57:40 -0300

    completado interface de retorno do tiny

--- src/routes/webhook/tiny/product/post.ts
+++ src/routes/webhook/tiny/product/post.ts
@@ -2,26 +2,32 @@ import {RouteHandlerMethod} from "fastify";
 import {getProduct} from "@api/tiny/product.js";
 
 interface ProductBody {
-	dados: {
-		idProduto: number;
-	};
+    cnpj: string;
+    tipo: string;
+    dados: {
+        sku: string;
+        nome: string;
+        saldo: number;
+        idProduto: number;
+    },
+    versao: string;
 }
 
 const postProduct: RouteHandlerMethod = async (request, reply) => {
-	try {
-		const {dados} = request.body as ProductBody;
-		const {idProduto} = dados;
-		
-		const data = await getProduct(idProduto)
-		if (!data) return reply.status(404).send({error: 'Product not found'})
-		if (data.retorno.erros) return reply.status(400).send({error: data.retorno.erros.map(e => e.erro).join(', ')})
-		
-		reply.server.db.insertFitnessProduct(data.retorno.produto)
-		
-		return reply.status(201).send({data: data.retorno.produto})
-	} catch (e) {
-		return reply.status(500).send({error: e})
-	}
+    try {
+        const {dados} = request.body as ProductBody;
+        const {idProduto} = dados;
+
+        const data = await getProduct(idProduto)
+        if (!data) return reply.status(404).send({error: 'Product not found'})
+        if (data.retorno.erros) return reply.status(400).send({error: data.retorno.erros.map(e => e.erro).join(', ')})
+
+        reply.server.db.insertFitnessProduct(data.retorno.produto)
+
+        return reply.status(201).send({data: data.retorno.produto})
+    } catch (e) {
+        return reply.status(500).send({error: e})
+    }
 }
 
 export default postProduct;
