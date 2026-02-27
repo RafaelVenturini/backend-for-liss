cc7ed41ce0a8d14c1d2be166d1517f5a4bf5ce10
Rafael Venturini <rafaelventurinidipalma@gmail.com>
2026-02-27 14:04:41 -0300

    funções que eu nao lembro o que fazem (MAS PRECISA DELAS!!!!!)

---
+++ src/lib/functions/nuvemshop/get-customer-data.ts
@@ -0,0 +1,49 @@
+import {getCustomer, getCustomerByIdentification,} from "@api/tiendanube/customer.js";
+
+interface GetCustomerData {
+    code: number;
+    msg?: string;
+    data?: any;
+}
+
+export async function getCustomerData(
+    id?: number | string | null,
+    identification?: string | number | null,
+): Promise<GetCustomerData> {
+    if (!id && !identification)
+        return {
+            code: 400,
+            msg: "Missing customer identification",
+        };
+    let data;
+    if (id) {
+        id = Number(id);
+        data = await getCustomer(id);
+    } else {
+        identification = String(identification);
+        data = await getCustomerByIdentification(identification);
+    }
+
+    if (!data.identification)
+        return {
+            code: 202,
+            msg: "Customer has no identification (CPF/CNPJ)",
+        };
+    if (!data)
+        return {
+            code: 404,
+            msg: "Customer not found",
+        };
+    const insert = {
+        cliente_id: data.identification,
+        nome: data.name,
+        dia_cadastro: data.created_at,
+        telefone: data.phone,
+        nuvem_id: data.id,
+        email: data.email,
+    };
+    return {
+        code: 200,
+        data: insert,
+    };
+}
