7638801663c1b630a0422315d3731d1768e4170d
Rafael Venturini <rafaelventurinidipalma@gmail.com>
2026-02-27 14:59:42 -0300

    adaptado envs para versão mais universal do railway

--- src/plugins/database.ts
+++ src/plugins/database.ts
@@ -1,15 +1,30 @@
 import fp from "fastify-plugin";
-import {createPool} from "mysql2/promise";
+import {createPool, PoolOptions, RowDataPacket} from "mysql2/promise";
 import {appConfig} from "@config";
 import {FastifyInstance} from "fastify";
 import {fitness} from "@/lib/database/fitness/_fitness-sql.js";
 import {tool} from "@/lib/database/tools/_tool.js";
 import {fashion} from "@/lib/database/fashion/_fashion.js";
 
+interface SqlTest extends RowDataPacket {
+    OK: "OK"
+}
+
+const testSql = "select 'ok'"
+
+async function createPoolAndTest(config: PoolOptions, name: string) {
+    const pool = createPool(config);
+    const [rows] = await pool.execute<SqlTest[]>(testSql);
+    console.log(`Database ${name} is ${rows[0].ok}`);
+    return pool;
+}
+
 async function databasePlugin(fastify: FastifyInstance) {
-    const fitnessPool = createPool(appConfig.databases.fitness);
-    const fashionPool = createPool(appConfig.databases.fashion);
-    const toolPool = createPool(appConfig.databases.tools);
+    console.log("Creating pools...")
+    const fitnessPool = await createPoolAndTest(appConfig.databases.fitness, "fitness");
+    const fashionPool = await createPoolAndTest(appConfig.databases.fashion, "fashion");
+    const toolPool = await createPoolAndTest(appConfig.databases.tools, "tools");
+
 
     fastify.decorate("db", {
         pool: {
